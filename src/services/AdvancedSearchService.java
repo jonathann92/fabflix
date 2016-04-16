201800a8249cbc436d345a962dd3637489866967
Jonathan Nguyen <jonathann92@gmail.com>
2016-04-15 17:08:32 -0700

    Included id in Advanced Search

--- src/services/AdvancedSearchService.java
+++ src/services/AdvancedSearchService.java
@@ -3,6 +3,7 @@ package services;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -15,10 +16,8 @@ import objects.Star;
 
 public class AdvancedSearchService extends Service{
 	
-	public static List<Movie> advSearch(String title, String year, String director, String first, String last) throws Exception{
-		if(title == null)
-			throw new Exception("No search arguments given");
-		
+	public static List<Movie> advSearch(String id, String title, String year, String director, String first, String last) throws Exception {
+		Set<Movie> movieList = new HashSet<Movie>();
 		String query1 = "select distinct movies.* from movies, stars, stars_in_movies " 
 		+ "where movies.title like '%"+ title + "%' "
 		+ "and movies.director like '%" + director +"%' "
@@ -26,9 +25,9 @@ public class AdvancedSearchService extends Service{
 		+ "and stars.first like '%" + first + "%' "
 		+ "and stars.last like '%" + last + "%' "
 		+ "and stars.id = stars_in_movies.star_id "
-		+ "and movies.id = stars_in_movies.movie_id"; 
-
-		Set<Movie> movieList = new HashSet<Movie>();
+		+ "and movies.id = stars_in_movies.movie_id "
+		+ (id.length() > 0 ? "and movies.id = " + id : "") ;
+		
 		movieList.addAll(queryMovieList(query1));
 		
 		if(first.length() != 0 && last.length() != 0){
@@ -36,7 +35,8 @@ public class AdvancedSearchService extends Service{
 			String query2 = "select distinct * from movies "
 			+ "where title like '%"+ title + "%' "
 			+ "and director like '%" + director +"%' "
-			+ "and year like '%" + year+ "%' ";
+			+ "and year like '%" + year+ "%' "
+			+ "and id = " + id;
 		
 			movieList.addAll(queryMovieList(query2));
 		}
@@ -47,26 +47,34 @@ public class AdvancedSearchService extends Service{
 		return new ArrayList<Movie>(movieList);
 	}
 	
-	private static Set<Movie> queryMovieList(String query) throws Exception{
+	private static Set<Movie> queryMovieList(String query) throws ClassNotFoundException, SQLException {
 		Set<Movie> movieList = new HashSet<Movie>();
 		Connection conn = null;
 		Statement select = null;
 		ResultSet rs = null;
 		
-		Class.forName(JDBC_DRIVER);
-		conn = DriverManager.getConnection("jdbc:mysql:///"+db, user, pass);
-		select = conn.createStatement();
-		rs = select.executeQuery(query);
 		
-		while(rs.next()){
-			Movie m = new Movie(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getString(4), rs.getString(5),
-					rs.getString(6));
-			movieList.add(m);
-		}
+		try {
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection("jdbc:mysql:///"+db, user, pass);
+			select = conn.createStatement();
+			rs = select.executeQuery(query);
+			
+			while(rs.next()){
+				Movie m = new Movie(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getString(4), rs.getString(5),
+						rs.getString(6));
+				movieList.add(m);
+			}
 
-		rs.close();
-		select.close();
-		conn.close();
+			rs.close();
+			select.close();
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			throw new ClassNotFoundException("Class no found " + JDBC_DRIVER);
+		} catch (SQLException e) {
+			throw new SQLException("No Server Connected");
+		}
+		
 		return movieList;
 	}
 	
--- src/servlet/AdvSearch.java
+++ src/servlet/AdvSearch.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Set;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -31,40 +32,83 @@ public class AdvSearch extends HttpServlet {
         PrintWriter out = response.getWriter();
         HttpSession session = request.getSession(true);
         
-        String error = "No error";
+        String error = null;
         
-        int id = 0;
-        String idParam = request.getParameter("id");
+        String id = request.getParameter("id");
         String title = request.getParameter("title");
         String year = request.getParameter("year");
         String director = request.getParameter("director");
         String first = request.getParameter("first");
         String last = request.getParameter("last");
-        String query = "title=" + title + "&year=" + year + "&director=" + director + "&first=" + first + "&last="+last;
         
-        try{
-        	id = Integer.parseInt(idParam);
-        } catch ( Exception e){
-        	error = "Id is not an integer";
-        	setParams(request, error, idParam, title, year, director, first, last);
-        	Service.forward(request, response, "/AdvancedSearch.jsp");
-        }
-        
-        
-        try{
+        if(checkParameters(request, response, id, title, year, director, first, last) && checkIntParam(request, response, id, title, year, director, first, last) )
+        {
+        	String query = "title=" + title + "&year=" + year + "&director=" + director + "&first=" + first + "&last="+last;
         	List<Movie> movieList;
-        	movieList = AdvancedSearchService.advSearch(title, year, director, first, last);
-        	session.setAttribute("fullMovieList", movieList);
-        	request.setAttribute("prevpage", "AdvSearch");
-        	request.setAttribute("query", query);
-        	Service.forward(request, response, "/MovieList");
-        	//response.sendRedirect("/filmdb/MovieList");
-        } catch (Exception e){
-        	error = e.getMessage();
-        	request.setAttribute("error", "SQL Server Down");
-        	Service.forward(request, response, "/AdvancedSearch.jsp");
-        }
+
+	    	try {
+				movieList = AdvancedSearchService.advSearch(id, title, year, director, first, last);
+				session.setAttribute("fullMovieList", movieList);
+		    	request.setAttribute("prevpage", "AdvSearch");
+		    	request.setAttribute("query", query);
+		    	Service.forward(request, response, "/MovieList");
+			} catch (Exception e) {
+				error = e.getMessage();
+	        	request.setAttribute("error", error);
+	        	Service.forward(request, response, "/AdvancedSearch.jsp");
+			}
+        } // END IF
+        
+//        try{
+//        	
+//        	
+//        	List<Movie> movieList;
+//        	movieList = AdvancedSearchService.advSearch(title, year, director, first, last);
+//        	session.setAttribute("fullMovieList", movieList);
+//        	request.setAttribute("prevpage", "AdvSearch");
+//        	request.setAttribute("query", query);
+//        	Service.forward(request, response, "/MovieList");
+//        	//response.sendRedirect("/filmdb/MovieList");
+//        } catch (Exception e){
+//        	error = e.getMessage();
+//        	request.setAttribute("error", "SQL Server Down");
+//        	Service.forward(request, response, "/AdvancedSearch.jsp");
+//        }
 	}
+	
+	private boolean checkParameters(HttpServletRequest request, HttpServletResponse response, String idParam, String title,
+			String year, String director, String first, String last) {
+		String error;
+		if(idParam.length() == 0 && title.length() == 0 && year.length() == 0 && director.length() == 0 &&  first.length() == 0 && last.length() == 0)
+        {
+        	error = "No parameters given";
+        	request.setAttribute("error", error);
+        	Service.forward(request, response, "/AdvancedSearch.jsp");
+        	return false;
+        }
+		return true;
+	}
+
+	private boolean checkIntParam(HttpServletRequest request, HttpServletResponse response, String idParam, String title,
+			String year, String director, String first, String last) throws IOException 
+	{
+        try{
+        	if(idParam.length() > 0)
+        		Integer.parseInt(idParam);
+        	if(year.length() > 0)
+        		Integer.parseInt(year);
+        } catch (Exception e){
+        	String error = "ID or Year is not an integer";
+        	setParams(request, error, idParam, title, year, director, first, last);
+        	request.setAttribute("error", error);
+        	Service.forward(request, response, "/AdvancedSearch.jsp");
+        	return false;
+        }
+        
+		return true;
+	}
+
+	
 
 	private void setParams(HttpServletRequest request, String error, String id, String title, String year, String director,
 			String first, String last) {
