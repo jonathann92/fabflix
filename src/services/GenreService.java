9cd090a88e3947d89061e177df2ef785d0e89adc
jegriffi <jegriffi@uci.edu>
2016-05-24 12:42:45 -0700

    GenreService PS

--- src/services/GenreService.java
+++ src/services/GenreService.java
@@ -2,6 +2,7 @@ package services;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -15,16 +16,16 @@ public class GenreService extends Service {
 	public static List<Genre> allGenres(){
 		List<Genre> genres = null;
 		Connection conn = null;
-		Statement select = null;
+		PreparedStatement select = null;
 		ResultSet rs = null;
 		
 		try {
-			Class.forName(JDBC_DRIVER);
+			Class.forName(JDBC_DRIVER);			
 			conn =  DriverManager.getConnection(DB_URL, user, pass);
-			select =  conn.createStatement();
-			genres = new ArrayList<Genre>();
 			String query = "select * from genres";
-			rs = select.executeQuery(query);
+			select =  (PreparedStatement) conn.prepareStatement(query);
+			genres = new ArrayList<Genre>();
+			rs = select.executeQuery();
 			
 			while(rs.next()){
 				Genre g = new Genre(rs.getInt(1), rs.getString(2));
@@ -41,18 +42,21 @@ public class GenreService extends Service {
 	public static List<Movie> moviesInGenre(int id){
 		List<Movie> movies = null;
 		Connection conn = null;
-		Statement select = null;
+		PreparedStatement select = null;
 		ResultSet rs = null;
 		
 		try {
 			Class.forName(JDBC_DRIVER);
 			conn =  DriverManager.getConnection(DB_URL, user, pass);
-			select =  conn.createStatement();
+			
 			movies = new ArrayList<Movie>();
 			String query = "select distinct movies.* from genres_in_movies, movies where "
-					+ id + " = genres_in_movies.genre_id "
+					+ "? = genres_in_movies.genre_id "
 					+ "and genres_in_movies.movie_id = movies.id";
-			rs = select.executeQuery(query);
+			
+			select =  conn.prepareStatement(query);
+			select.setInt(1, id);
+			rs = select.executeQuery();
 			
 			while(rs.next()){
 				Movie m = new Movie(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getString(4), rs.getString(5), rs.getString(6));
@@ -73,18 +77,20 @@ public class GenreService extends Service {
 	public static List<Movie> moviesInGenre(String name){
 		List<Movie> movies = null;
 		Connection conn = null;
-		Statement select = null;
+		PreparedStatement select = null;
 		ResultSet rs = null;
 		
 		try {
 			Class.forName(JDBC_DRIVER);
 			conn =  DriverManager.getConnection(DB_URL, user, pass);
-			select =  conn.createStatement();
+			
 			movies = new ArrayList<Movie>();
 			String query = "select distinct movies.* from genres, genres_in_movies, movies where "
-							+ "genres.name = '" + name + "' and genres.id = genres_in_movies.genre_id "
+							+ "genres.name = ? and genres.id = genres_in_movies.genre_id "
 							+ "and genres_in_movies.movie_id = movies.id";
-			rs = select.executeQuery(query);
+			select =  conn.prepareStatement(query);
+			select.setString(1, name);
+			rs = select.executeQuery();
 			
 			while(rs.next()){
 				Movie m = new Movie(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getString(4), rs.getString(5), rs.getString(6));
