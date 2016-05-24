7eda98c7d4a1203348f7ee612639b82eba433eba
jegriffi <jegriffi@uci.edu>
2016-05-24 12:35:58 -0700

    Updated EmployeeService

--- src/services/EmployeeService.java
+++ src/services/EmployeeService.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+
+import com.mysql.jdbc.PreparedStatement;
+
 import objects.Employee;
 
 public class EmployeeService extends Service{
@@ -14,14 +17,13 @@ public class EmployeeService extends Service{
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		
-		String query = "select * from employees "
-				+ "where email='?' and password ='?';";
+		String query = "select * from employees where email=? and password=?;";
 		
 		conn = DriverManager.getConnection("jdbc:mysql:///"+db, user, pass);
-		stmt = conn.preparedStatement(query);
+		stmt = (PreparedStatement) conn.prepareStatement(query);
 		stmt.setString(1, email);
 		stmt.setString(2, password);
-		rs = stmt.executeQuery(query);
+		rs = stmt.executeQuery();
 		while(rs.next()) {
 			emp =  new Employee(rs.getString(1), rs.getString(2), rs.getString(3));
 			if (password.equals(rs.getString(2)) && email.equals(rs.getString(1))) {
